b097d2c01847b2bd94c902d1681169802685316c
matthijs.vanHenten <matthijs.vanHenten@f27d9930-b0da-11de-a90c-f79689ea6043>
2010-08-29 00:11:49 +0000

    added linktree controller
    git-svn-id: http://mvh-source.googlecode.com/svn/trunk/pico@274 f27d9930-b0da-11de-a90c-f79689ea6043

---
+++ Application/modules/Admin/controller/Link.php
@@ -0,0 +1,42 @@
+<?php
+class Controller_Admin_Link extends Pico_AdminController{
+    protected function indexAction(){
+        $this->_forward('list');
+    }
+
+    protected  function listAction(){
+        $request = $this->getRequest();
+        $links   = array();
+
+        if( $request->item ){
+            $item = Model_Link::get( $request->item );
+            $items = Model_Link::get()->all()
+                    ->where( 'group', $item->group );
+        }
+        else if( $request->id ){
+            $items = Model_Link::get()->all()
+                    ->where( 'group', $request->id )
+                    ->where( 'parent_id', 0 );
+
+            foreach( $items as $item ){
+                $href = $this->getView()->url(array('action'=>'edit', 'id'=>$item->id));
+                $links[] = $this->getView()->link( $href,$item->title );
+            }
+
+            $this->getView()->mainLeft = join('\n', $links );
+        }
+    }
+
+    protected function editAction(){
+        $request = $this->getRequest();
+
+        $item = Model_Link::get( $request->id );
+        $items = Model_Link::get()->all()
+                ->where( 'group', $item->group )
+                ->order( 'parent_id');
+
+        $this->getView()->mainLeft = $this->getView()->linkTree( $items, $item );
+
+
+    }
+}
---
+++ Application/modules/Admin/helper/LinkTree.php
@@ -0,0 +1,63 @@
+<?php
+class Helper_LinkTree extends Nano_View_Helper{
+    function LinkTree( $items, $current ){
+        $sorted = $this->sortItems( $items, $current );
+        $ul     = new Nano_Element( 'ul', array('class'=>'pico-linktree'));
+
+        foreach( $sorted as $parent ){
+            $ul->addChild( $this->renderItem( $parent, (int) $current->id ));
+        }
+
+        return $ul;
+    }
+
+    private function renderItem( $item, $current ){
+        $html = array();
+
+        $url = $this->getView()->url( array( 'id' => $item->id ) );
+
+        $li = new Nano_Element('li', array('class' => ( $item->active ? 'active':'')));
+        $li->addChild( new Nano_Element( 'a', array('href' => $url ), $item->title ) );
+
+        if( $item->active && count( $item->children ) && $item->id !== $current ){
+            $ul = new Nano_Element( 'ul' );
+
+            foreach( $item->children as $child ){
+                $ul->addChild( $this->renderItem( $child, $current ) );
+            }
+            $li->addChild( $ul );
+        }
+        return $li;
+    }
+
+    private function sortItems( $items, $item ){
+        $sort = array();
+        foreach( $items as $obj ) $sort[$obj->id] = (object) $obj->properties();
+
+        $sorted = array();
+        foreach( $sort as $id => $obj ){
+            $id = (int) $id;
+            $obj->parent_id = (int) $obj->parent_id;
+            $obj->active = $id == $item->id ? true : false;
+            $obj->children = array();
+
+            if( $obj->parent_id > 0 ){
+                $sort[$obj->parent_id]->children[$id] = $obj;
+                if( $obj->active ){
+                    // walk up to activate parents
+                    while( $obj->parent_id > 0 ){
+                        $obj = $sort[$obj->parent_id];
+                        $obj->active = true;
+                    }
+
+                }
+            }
+            else{
+                $sorted[$id] = $obj;
+            }
+
+        }
+
+        return $sorted;
+    }
+}
