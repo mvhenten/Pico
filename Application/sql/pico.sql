3c062458cccd44af59d5e7998c89e38d2a9463ae
matthijs.vanHenten <matthijs.vanHenten@f27d9930-b0da-11de-a90c-f79689ea6043>
2010-09-10 00:07:17 +0000

    db layout
    git-svn-id: http://mvh-source.googlecode.com/svn/trunk/pico@280 f27d9930-b0da-11de-a90c-f79689ea6043

--- Application/sql/pico.sql
+++ Application/sql/pico.sql
@@ -1,7 +1,26 @@
-SET FOREIGN_KEY_CHECKS = 0;
+-- phpMyAdmin SQL Dump
+-- version 3.2.0
+-- http://www.phpmyadmin.net
+--
+-- Host: localhost
+-- Generation Time: Sep 10, 2010 at 02:05 AM
+-- Server version: 5.1.41
+-- PHP Version: 5.3.2-1ubuntu4.2
 
-CREATE TABLE `image_data` (
-  `id` int(10) unsigned NOT NULL auto_increment,
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+
+--
+-- Database: `pico`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `image_data`
+--
+
+CREATE TABLE IF NOT EXISTS `image_data` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `image_id` int(10) unsigned NOT NULL,
   `size` int(10) unsigned zerofill NOT NULL,
   `width` int(10) unsigned zerofill NOT NULL,
@@ -9,28 +28,68 @@ CREATE TABLE `image_data` (
   `type` int(10) unsigned zerofill NOT NULL,
   `mime` varchar(255) NOT NULL,
   `filename` varchar(1024) NOT NULL,
-  `data` text,
-  PRIMARY KEY  (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=54 DEFAULT CHARSET=latin1;
+  `data` longblob,
+  `created` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00' ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=303 ;
 
+-- --------------------------------------------------------
 
-CREATE TABLE `item` (
-  `id` int(10) unsigned NOT NULL auto_increment,
-  `name` varchar(255) default NULL,
-  `description` varchar(2048) default NULL,
-  `type` int(11) unsigned default NULL,
-  `visible` tinyint(4) unsigned zerofill default NULL,
-  `updated` timestamp NULL default NULL on update CURRENT_TIMESTAMP,
-  `inserted` timestamp NULL default NULL,
-  PRIMARY KEY  (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=16 DEFAULT CHARSET=latin1;
+--
+-- Table structure for table `image_label`
+--
 
-
-CREATE TABLE `image_label` (
-  `image_id` int(11) unsigned NOT NULL,
-  `label_id` int(11) unsigned NOT NULL,
+CREATE TABLE IF NOT EXISTS `image_label` (
+  `image_id` int(10) unsigned NOT NULL,
+  `label_id` int(10) unsigned NOT NULL,
+  `priority` int(10) unsigned zerofill DEFAULT '0000000000',
   UNIQUE KEY `image_label` (`image_id`,`label_id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
+-- --------------------------------------------------------
 
-SET FOREIGN_KEY_CHECKS = 1;
+--
+-- Table structure for table `item`
+--
+
+CREATE TABLE IF NOT EXISTS `item` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  `description` varchar(2048) DEFAULT NULL,
+  `type` int(11) unsigned NOT NULL,
+  `data` text,
+  `visible` tinyint(4) unsigned zerofill DEFAULT '0000',
+  `updated` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00' ON UPDATE CURRENT_TIMESTAMP,
+  `inserted` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=140 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `link`
+--
+
+CREATE TABLE IF NOT EXISTS `link` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `parent_id` int(10) unsigned zerofill NOT NULL DEFAULT '0000000000',
+  `priority` int(10) unsigned DEFAULT '0',
+  `title` varchar(255) DEFAULT NULL,
+  `url` varchar(4096) DEFAULT NULL,
+  `description` varchar(4096) DEFAULT NULL,
+  `group` int(10) unsigned NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=36 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `link_group`
+--
+
+CREATE TABLE IF NOT EXISTS `link_group` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  `description` varchar(1024) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
