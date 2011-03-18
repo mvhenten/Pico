20e93686c953c5bdd45fd89650dac0c54cdc7513
Matthijs van Henten <matthijs@ischen.nl>
2011-03-19 00:03:36 +0100

    refactored a bit: moved common cases into common, and content as a dedicated view

---
+++ Application/admin/controller/Common.php
@@ -0,0 +1,52 @@
+<?php
+class Controller_Admin_Common extends Nano_Controller{
+    public function post( $request, $config ){
+        $post = $request->getPost();
+
+        if( $request->action == 'delete' && $post->confirm ){
+            $item = new Model_Item($request->id);
+            $type = $item->type;
+
+            if( $post->confirm ){
+                $item->delete();
+            }
+
+            //$action = $type == 'label' ? 'label' : 'list';
+            //$this->response()->redirect('/admin/' . $type . '/' . $action );
+            $view = $type == 'label' ? 'image/label' : $type;
+            $this->response()->redirect( '/admin/' . $view );
+
+
+        }
+    }
+
+    public function getDelete( $request, $config ){
+        if( $request->id ){
+            $item = new Model_Item($request->id);
+
+            $this->template()->item = $item;
+            return $this->template()->render('admin/template/common/delete');
+        }
+    }
+    public function getAdd( $request, $config ){
+        $item = new Model_Item( array(
+            'name'          => 'Untitled new ' . $request->id,
+            'description'   => 'Description for untitled new ' . $request->id,
+            'type'           => $request->id
+        ));
+
+        $item->put();
+
+        if( $item->type !== 'label' ){
+            $content = new Model_ItemContent();
+            $content->item_id = $item->id;
+            $content->put();
+        }
+
+        $view = $item->type == 'label' ? 'image/label' : $item->type;
+
+        $this->response()->redirect( '/admin/' . $view );
+    }
+
+
+}
