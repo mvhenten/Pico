1f4e08a656ef1c98aaf1a67f9de5a711f5f17d89
Matthijs van Henten <matthijs@ischen.nl>
2011-03-19 01:42:03 +0100

    refactored code to go into nano.js. implemented sorting images within labels

---
+++ www/js/nano.js
@@ -0,0 +1,129 @@
+var nano = {
+    ahah: {
+        /**
+         * shows the link in a dialog
+         */
+        link: function( link, selector ){
+            var url = link.href;
+
+            $.ajax({
+                url: url,
+                type: "GET",
+                context: document.body,
+                success: function( data, status, xhr ){
+                    $(data).find(selector).dialog({
+                        modal:true
+                    });
+                }
+            });
+
+            return false;
+        },
+
+        /**
+         * override form submit handler: post the form and display the
+         * results in a dialog
+         */
+        form: function( form, selector ){
+            var url     = $(form).attr('action');
+            var mtd  = $(form).attr('method');
+
+            $.ajax({
+                url: url,
+                type: mtd,
+                context: document.body,
+                data: $(form).serialize(),
+                success: function( data, status, xhr ){
+                    $(data).find(selector).dialog({
+                        modal:true
+                    });
+                }
+            });
+
+            return false;
+        }
+    },
+
+    validate: {
+      /**
+       * simple validation: checks for file types
+       * @todo Check for size, too
+       */
+      upload: function( el, types, label, buttons ){
+        var ext = el.value.toLowerCase().split('.').pop();
+        console.log(label);
+        console.log(buttons);
+        label = $(el.form).find(label);
+        buttons = $(el.form).find(buttons);
+
+        var validate = {};
+        for( i in types ) validate[types[i]] = true;
+
+        console.log(validate);
+        console.log(label);
+        console.log(buttons);
+
+        if( ext in validate ){
+            $(label).text("Selected " + el.value)
+            $(buttons).removeAttr('disabled');
+        }
+        else{
+            el.value = null;
+            $(label).text('Invalid image type: ' + ext)
+            $(buttons).attr('disabled','disabled');
+        }
+      }
+    }
+}
+
+
+//$(function(){
+//    $('#action-labels').bind('click', function(evt){
+//        var url = $('#images').attr('action');
+//        $.ajax({url:url,
+//            context: document.body,
+//            type: "POST",
+//            data: $('#images').serialize(),
+//            success: function( data, status, xhr ){
+//                $($(data).find('form')[0]).dialog({
+//                    modal:true
+//                });
+//            }
+//        });
+//        return false;
+//    }, false);
+//});
+
+function upload_validate(el){
+    var ext = el.value.toLowerCase().split('.').pop();
+
+    console.log(ext in {'png':1,'jpeg':1,'jpg':1})
+
+    if( ext in {'png':1,'jpeg':1,'jpg':1} ){
+        $(el.parentNode).find('label').text("Selected " + el.value)
+        $('.input-submit').removeAttr('disabled');
+    }
+    else{
+        el.value = null;
+        $(el.parentNode).find('label').text('Invalid image type: ' + ext)
+        $('.input-submit').attr('disabled','disabled');
+    }
+}
+
+function ajaxify( link, search ){
+    var url = link.href;
+
+    $.ajax({
+        url: url,
+        context: document.body,
+        type: "GET",
+        success: function( data, status, xhr ){
+            $(data).find(search).dialog({
+                title: '',
+                modal:true
+            });
+        }
+    });
+    return false;
+
+}
