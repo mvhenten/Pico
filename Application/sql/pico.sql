54815f6281d0a1b66a28aefbf6da2ac6bbefecfa
Matthijs van Henten <matthijs@waag.org>
2011-04-02 17:50:52 +0200

    updated db schema. added slug

--- Application/sql/pico.sql
+++ Application/sql/pico.sql
@@ -1,27 +1,23 @@
--- MySQL dump 10.13  Distrib 5.1.41, for debian-linux-gnu (x86_64)
---
--- Host: localhost    Database: pico
--- ------------------------------------------------------
--- Server version	5.1.41-3ubuntu12.10
+/*
+Navicat MySQL Data Transfer
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+Source Server         : localhost
+Source Server Version : 50141
+Source Host           : localhost:3306
+Source Database       : wedding
 
---
--- Table structure for table `image_data`
---
+Target Server Type    : MYSQL
+Target Server Version : 50141
+File Encoding         : 65001
 
+Date: 2011-03-30 00:34:27
+*/
+
+SET FOREIGN_KEY_CHECKS=0;
+-- ----------------------------
+-- Table structure for `image_data`
+-- ----------------------------
 DROP TABLE IF EXISTS `image_data`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `image_data` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `image_id` int(10) unsigned NOT NULL,
@@ -35,33 +31,34 @@ CREATE TABLE `image_data` (
   `created` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00' ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`)
 ) ENGINE=MyISAM AUTO_INCREMENT=60 DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
---
--- Table structure for table `image_label`
---
+-- ----------------------------
+-- Records of image_data
+-- ----------------------------
 
+-- ----------------------------
+-- Table structure for `image_label`
+-- ----------------------------
 DROP TABLE IF EXISTS `image_label`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `image_label` (
   `image_id` int(11) unsigned NOT NULL,
   `label_id` int(11) unsigned NOT NULL,
   `priority` int(10) unsigned zerofill NOT NULL,
   UNIQUE KEY `image_label` (`image_id`,`label_id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
---
--- Table structure for table `item`
---
+-- ----------------------------
+-- Records of image_label
+-- ----------------------------
 
+-- ----------------------------
+-- Table structure for `item`
+-- ----------------------------
 DROP TABLE IF EXISTS `item`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `item` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `type` varchar(255) NOT NULL,
+  `slug` varchar(64) NOT NULL,
   `parent` int(10) unsigned zerofill DEFAULT NULL,
   `visible` tinyint(4) unsigned zerofill DEFAULT NULL,
   `name` varchar(255) DEFAULT NULL,
@@ -71,15 +68,15 @@ CREATE TABLE `item` (
   `inserted` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
   PRIMARY KEY (`id`)
 ) ENGINE=MyISAM AUTO_INCREMENT=51 DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
---
--- Table structure for table `item_content`
---
+-- ----------------------------
+-- Records of item
+-- ----------------------------
 
+-- ----------------------------
+-- Table structure for `item_content`
+-- ----------------------------
 DROP TABLE IF EXISTS `item_content`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `item_content` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `item_id` int(10) unsigned NOT NULL,
@@ -88,15 +85,15 @@ CREATE TABLE `item_content` (
   `updated` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00' ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`)
 ) ENGINE=MyISAM AUTO_INCREMENT=19 DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
---
--- Table structure for table `link`
---
+-- ----------------------------
+-- Records of item_content
+-- ----------------------------
 
+-- ----------------------------
+-- Table structure for `link`
+-- ----------------------------
 DROP TABLE IF EXISTS `link`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `link` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `group` varchar(255) NOT NULL,
@@ -107,30 +104,30 @@ CREATE TABLE `link` (
   `description` varchar(4096) DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=MyISAM AUTO_INCREMENT=38 DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
---
--- Table structure for table `link_group`
---
+-- ----------------------------
+-- Records of link
+-- ----------------------------
 
+-- ----------------------------
+-- Table structure for `link_group`
+-- ----------------------------
 DROP TABLE IF EXISTS `link_group`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `link_group` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `name` varchar(255) NOT NULL,
   `description` varchar(1024) NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
---
--- Table structure for table `setting`
---
+-- ----------------------------
+-- Records of link_group
+-- ----------------------------
 
+-- ----------------------------
+-- Table structure for `setting`
+-- ----------------------------
 DROP TABLE IF EXISTS `setting`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `setting` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `group` varchar(255) NOT NULL,
@@ -142,15 +139,7 @@ CREATE TABLE `setting` (
   `updated` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00' ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`)
 ) ENGINE=MyISAM AUTO_INCREMENT=8 DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
--- Dump completed on 2011-03-14  0:20:52
+-- ----------------------------
+-- Records of setting
+-- ----------------------------
