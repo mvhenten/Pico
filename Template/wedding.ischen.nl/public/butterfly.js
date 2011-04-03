d173f92b4eeb4fef6e924f77d1ec0ed2dacf622a
Matthijs van Henten <matthijs@waag.org>
2011-04-03 23:36:28 +0200

    added view for images and implemented ahah popup

---
+++ Template/wedding.ischen.nl/public/butterfly.js
@@ -0,0 +1,12 @@
+(function( $ ){
+    var methods = {
+        init: function(){
+            
+        }
+
+    }
+
+    $.fn.butterfly = function() {
+        return methods.init.apply( this, arguments );
+    };
+})( jQuery );
