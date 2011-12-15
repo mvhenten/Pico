85bed595f0c34568af5c18ec8c10e5e7080d6931
Matthijs van Henten <matthijs@ischen.nl>
2011-12-16 00:59:30 +0100

    tidied

--- Application/plugin/ItemThumbnail/hook.php
+++ Application/plugin/ItemThumbnail/hook.php
@@ -1,17 +1,25 @@
-<?php @list( , $controller, $action, $item_id ) = $request->pathParts ?>
+<?php
+/**
+ * Application/plugin/ItemThumbnail/hook.php
+ *
+ * @package default
+ */
 
-<?php if( $start ):
-    Nano_Autoloader::registerNamespace( 'ItemThumb', dirname(__FILE__)  );
-    $this->router->addRoute( '/admin/plugin/thumb/\w+(/\d+)?', 'ItemThumb_Plugin' );
 
-    if( $action && $action == 'delete' ){
-        $image_data = new Pico_Model_ImageData();
-        $image_data->delete( array( 'image_id' => $item_id ) );
-    }
+@list( , $controller, $action, $item_id ) = $request->pathParts ?>
+
+<?php if ( $start ):
+	Nano_Autoloader::registerNamespace( 'ItemThumb', dirname(__FILE__)  );
+$this->router->addRoute( '/admin/plugin/thumb/\w+(/\d+)?', 'ItemThumb_Plugin' );
+
+if ( $action && $action == 'delete' ) {
+	$image_data = new Pico_Model_ImageData();
+	$image_data->delete( array( 'image_id' => $item_id ) );
+}
 endif; ?>
 
-<?php if( $end ): ?>
-    <?php if( $action == 'edit' && in_array( $controller, array('page', 'label')) ): ?>
+<?php if ( $end ): ?>
+    <?php if ( $action == 'edit' && in_array( $controller, array('page', 'label')) ): ?>
     <?php $this->template->block( 'head' ); ?>
     <style>
     <?php readfile( dirname(__FILE__) .  '/style.css' ); ?>
@@ -23,7 +31,7 @@ endif; ?>
     <li><h4><?php echo ucfirst($controller) ?> thumbnail</h4></li>
         <li>
             <div class="thumbnail-holder">
-                <?php if( isset($item->appendix->thumbnail) ): ?>
+                <?php if ( isset($item->appendix->thumbnail) ): ?>
                 <img src="/image/original/<?php echo $item->id ?>?v=<?php echo $item->appendix->thumbnail ?>" />
                 <?php else: ?>
                     <em>no image found</em>
@@ -33,7 +41,7 @@ endif; ?>
         <li>
             <form action="/admin/plugin/thumb/upload/<?php echo $item->id ?>" method="post" enctype="multipart/form-data">
                 <div class="fakefile">
-                    <?php if( isset($item->appendix->thumbnail) ): ?>
+                    <?php if ( isset($item->appendix->thumbnail) ): ?>
                     <a class="button" href="/admin/plugin/thumb/delete/<?php echo $item->id ?>" title="delete thumbnail">delete</a>
                     <label for="thumbnail-file"><span>upload...</span>
                     <?php else: ?>
