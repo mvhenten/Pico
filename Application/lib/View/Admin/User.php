45adeea2e0a3d626df088453fa46b77173d8a0f9
Matthijs van Henten <matthijs@ischen.nl>
2012-01-15 16:45:26 +0100

    fixed login form

--- Application/lib/View/Admin/User.php
+++ Application/lib/View/Admin/User.php
@@ -1,35 +1,99 @@
 <?php
-class Pico_View_Admin_User extends Nano_App_View{
-    public function post( Nano_App_Request $request, $config ){
-        $post = $request->post();
+/**
+ * Application/lib/View/Admin/User.php
+ *
+ * @author Matthijs van Henten <matthijs@ischen.nl>
+ * @package Bison
+ */
 
-        if( $post->username != $config->admin->username ){
-            $this->response()->redirect( '/admin/user/login?error=1' );
+
+class Pico_View_Admin_User extends Nano_App_View{
+
+    /**
+     *
+     *
+     * @param object  $request
+     * @param unknown $config
+     */
+    public function post( Nano_App_Request $request, $config ) {
+        @list( , , $action, $id ) = $request->pathParts();
+
+        $config = $config['config'];
+
+        if ( $action !== 'login' ) {
+            $this->response()->redirect( '/admin/user/login' );
         }
 
-        if( crypt( $post->password, $config->admin->password ) == $config->admin->password ){
-            @session_start();
-            $_SESSION['user'] = 1;
-            $this->response()->redirect( '/admin/' );
+        $post = $request->post;
+
+        if ( $post['username'] == $config->admin->username ) {
+
+            if ( crypt($post['password'], $config->admin->password) == $config->admin->password ) {
+                @session_start();
+                $_SESSION['user'] = 1;
+                $this->response()->redirect( '/admin/' );
+            }
         }
 
         $this->response()->redirect( '/admin/user/login?error=1' );
     }
 
-    public function get( Nano_App_Request $request, $config ){
-        @session_start();
 
-        if( $request->action == 'logout' ){
-            @session_destroy();
+    /**
+     *
+     *
+     * @param object  $request
+     * @param unknown $config
+     * @return unknown
+     */
+    public function get( Nano_App_Request $request, $config ) {
+        @list( , , $action ) = $request->pathParts();
 
-            $this->response()->redirect( '/admin/user/login' );
+        if ( $action == 'login' ) {
+            return $this->login( $request, $config );
         }
 
-        if( isset($_SESSION['user']) ){
+        if ( $action == 'logout' ) {
+            return $this->logout( $request, $config );
+        }
+
+        $this->response()->redirect( '/' );
+    }
+
+
+    /**
+     *
+     *
+     * @param object  $request
+     * @param unknown $config
+     * @return unknown
+     */
+    public function login( Nano_App_Request $request, $config ) {
+        @session_start();
+
+        if ( isset($_SESSION['user']) ) {
             $this->response()->redirect( '/admin/' );
         }
 
         return $this->template()->render( APPLICATION_ROOT . '/Admin/template/user');
     }
+
+
+
+    /**
+     *
+     *
+     * @param object  $request
+     * @param unknown $config
+     */
+    public function logout( Nano_App_Request $request, $config ) {
+        @session_start();
+
+        session_destroy();
+
+
+        $this->response()->redirect( '/admin/user/login' );
+    }
+
 
 }
