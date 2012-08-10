3e9b48264646dd21925e9be42157ceca922ecb09
Matthijs van Henten <matthijs@ischen.nl>
2012-08-11 01:57:32 +0200

    tidy

--- Application/plugin/Navtree/Model/Navtree.php
+++ Application/plugin/Navtree/Model/Navtree.php
@@ -1,14 +1,29 @@
 <?php
-class Navtree_Model_Navtree extends Pico_Schema_Item {    
-    function items(){
+/**
+ * Application/plugin/Navtree/Model/Navtree.php
+ *
+ * @author Matthijs van Henten <matthijs@ischen.nl>
+ * @package Pico
+ */
+
+
+class Navtree_Model_Navtree extends Pico_Schema_Item {
+
+
+    /**
+     *
+     *
+     * @return unknown
+     */
+    function items() {
         $model = new Pico_Schema_Item();
-        
+
         return $model->search(array(
-            'where' => array(
-                'type' => 'navigation',
-            ),
-            'order' => 'parent'//array( 'parent', 'priority' )
-        ));        
+                'where' => array(
+                    'type' => 'navigation',
+                ),
+                'order' => 'parent'//array( 'parent', 'priority' )
+            ));
     }
 
 
--- Application/plugin/Navtree/t/Model.php
+++ Application/plugin/Navtree/t/Model.php
@@ -1,5 +1,13 @@
 <?php
-    class Pico_Test_testNavtree extends PHPUnit_Framework_TestCase{
+/**
+ * Application/plugin/Navtree/t/Model.php
+ *
+ * @author Matthijs van Henten <matthijs@ischen.nl>
+ * @package Pico
+ */
+
+
+class Pico_Test_testNavtree extends PHPUnit_Framework_TestCase{
 
     /**
      * load pico/nano library
@@ -17,41 +25,47 @@
         $dbh = Nano_Db::getAdapter();
         $dbh->query( 'INSERT INTO item ( id, slug, type ) VALUES ( 1, "nav", "navigation")');
 
-        $parents = range(2,4);
-        
-        foreach( $parents as $pri => $parent ){
+        $parents = range(2, 4);
+
+        foreach ( $parents as $pri => $parent ) {
             $slug = join('_', array('parent', $pri, $parent) );
             $item = new Pico_Model_Item(array(
-                'parent' => 1,
-                'slug' => $slug,
-                'type' => 'navigation',
-                'priority' => $pri,
-                'id' => $parent            
-            ));
-                        
+                    'parent' => 1,
+                    'slug' => $slug,
+                    'type' => 'navigation',
+                    'priority' => $pri,
+                    'id' => $parent
+                ));
+
             $item->store();
         }
 
-        foreach( $parents as $pri => $parent ){
+        foreach ( $parents as $pri => $parent ) {
             $slug = join('_', array('child', $pri, $parent) );
             $item = new Pico_Model_Item(array(
-                'parent' => $parent,
-                'slug' => $slug,
-                'type' => 'navigation',
-                'priority' => $pri  ,       
-                'id' => $parent + 10           
-            ));
-                        
+                    'parent' => $parent,
+                    'slug' => $slug,
+                    'type' => 'navigation',
+                    'priority' => $pri  ,
+                    'id' => $parent + 10
+                ));
+
             $item->store();
         }
     }
 
+
+    /**
+     *
+     */
     public function test_construct() {
         $tree = new  Navtree_Model_Navtree(1);
-                
-        foreach( $tree->items() as $item ){
+
+        foreach ( $tree->items() as $item ) {
             var_dump( $item->slug );
             $this->assertInstanceOf( 'Pico_Schema_Item', $item );
         }
     }
+
+
 }
