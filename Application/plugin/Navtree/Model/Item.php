3310a1d45681d42204f24cd621a722561ee5ebc6
Matthijs van Henten <matthijs@ischen.nl>
2012-08-14 00:06:59 +0200

    tidy

--- Application/plugin/Navtree/Model/Item.php
+++ Application/plugin/Navtree/Model/Item.php
@@ -1,7 +1,20 @@
 <?php
+/**
+ * Application/plugin/Navtree/Model/Item.php
+ *
+ * @author Matthijs van Henten <matthijs@ischen.nl>
+ * @package Pico
+ */
+
 
 class Navtree_Model_Item extends Pico_Schema_Item {
 
+
+    /**
+     * Return all items that are children of this item
+     *
+     * @return PdoStatement $items
+     */
     function items() {
         return $this->has_many( 'Pico_Schema_Item', array(
                 'type' => 'type'
