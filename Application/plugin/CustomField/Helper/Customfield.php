4c006f9ff1849fe80d6ed0efb81a4ce0bb08f2ea
Matthijs van Henten <matthijs@ischen.nl>
2013-01-30 00:23:29 +0100

    return null for customfields if none

--- Application/plugin/CustomField/Helper/Customfield.php
+++ Application/plugin/CustomField/Helper/Customfield.php
@@ -1,17 +1,35 @@
 <?php
+/**
+ * Application/plugin/CustomField/Helper/Customfield.php
+ *
+ * @author Matthijs van Henten <matthijs@ischen.nl>
+ * @package Pico
+ */
+
+
 class CustomField_Helper_CustomField extends Nano_View_Helper {
 
+
+    /**
+     *
+     *
+     * @param unknown $item
+     * @param unknown $field_name
+     * @return unknown
+     */
     function CustomField( $item, $field_name ) {
         $appendix = $item->appendix;
 
-        if( ! isset( $appendix->custom_fields ) ){
-            return '';
+        if ( ! isset( $appendix->custom_fields ) ) {
+            return null;
         }
-        
-        if( ! isset( $appendix->custom_fields->$field_name ) ){
-            return '';
+
+        if ( ! isset( $appendix->custom_fields->$field_name ) ) {
+            return null;
         }
-        
+
         return htmlentities( $appendix->custom_fields->$field_name );
     }
+
+
 }
