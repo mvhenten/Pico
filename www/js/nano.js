0db112113fbb28aa6ca2223ee84836e3e24c8170
Matthijs van Henten <matthijs@ischen.nl>
2013-03-05 23:26:40 +0100

    cleanup js, remove modal window stuff

--- www/js/nano.js
+++ www/js/nano.js
@@ -1,119 +1,50 @@
-(function($){
-    var formAction = function( modalConfig ){
-        var $form = $( this.form ), formData = $form.serializeArray();
+(function($) {
+    var ahah = function(form, callback, options ) {
+        $.ajax({
+            url:  form.action,
+            type: form.method,
+            data: $(form).serializeArray(),
+            success: function(data) {
+                if (!data) return;
+                
+                var html = $(data).find(options.selector);
+                $( html ).addClass('ahah-generated');
+                $( options.target ).prepend(html);
+                
+                if (typeof callback === 'function') {
+                    callback( html );
+                }
+            }
+        })
+    }
+
+    $.fn.nanoAhahForm = function(callback, options) {
+        var _self = this;
         
-        formData.push({
-            name: $(this).attr('name'),
-            value: $(this).val()
+        options = $.extend({
+            selector: 'form',
+            parent: null,
+        },
+        options);
+
+        $(this).find('input[type="submit"], button').click(function(evt) {
+            $(_self).append(
+                $('<input id="ahah-cache"/>').attr({
+                    type: 'hidden',
+                    name: this.name,
+                    value: this.value
+                }));
+            });
+
+        $(this).submit(function(evt) {
+            evt.preventDefault();
+            options.target = options.target || $(this).first().parent();            
+            $('.ahah-generated').fadeOut().remove();
+
+            ahah(this, callback, options );
+            $('#ahah-cache').remove();
         });
 
-        $.ajax({
-            url: $form.attr('action'),
-            type: $form.attr('method'),
-            context: document.body,
-            data: formData,
-            success: function( data, status, xhr ){               
-                if( !data ) return;
-                
-                var html = $(data).find('form');
-                
-                modalConfig = $.extend({
-                    title: 'Select options',
-//                    position: { my: "center", at: "center", of: '#main' },
-                    modal: true,
-                    width: 600,
-                    height: 500,
-                    resizable: false,
-                    buttons: [
-                        { text: "Cancel", click: function() {
-                            $( this ).dialog( "close" );
-                        }},
-                        { text: "Save", click: function() {
-                            $(html).submit();
-                            $( this ).dialog( "close" );
-                        }}
-                    ]
-                }, modalConfig );
-                
-                $(html).dialog( modalConfig );            
-            }
-        });        
-    };
-    
-    $.fn.extend({
-        nanoAhahForm: function( targetSelector, modalConfig ) {
-            $( this ).click( function( evt ){
-                evt.preventDefault();
-                formAction.call( this, targetSelector, modalConfig );
-            });
-
-            return this;
-        }
-    });
+        return this;
+    }
 })(jQuery);
-
-var nano = {
-    ahah: {
-        /**
-         * shows the link in a dialog
-         */
-        link: function( link, selector ){
-            var url = link.href;
-
-            $.ajax({
-                url: url,
-                type: "GET",
-                context: document.body,
-                success: function( data, status, xhr ){
-                    $(data).find(selector).dialog({
-                        modal:true,
-                        height: 'auto',
-                        maxHeight: 500
-                    });
-                }
-            });
-
-            return false;
-        }
-    },
-
-    validate: {
-      /**
-       * simple validation: checks for file types
-       * @todo Check for size, too
-       */
-      upload: function( el, types, label, buttons ){
-        var ext = el.value.toLowerCase().split('.').pop();
-        label = $(el.form).find(label);
-        buttons = $(el.form).find(buttons);
-
-        var validate = {};
-        for( i in types ) validate[types[i]] = true;
-
-
-        if( ext in validate ){
-            $(label).text("Selected " + el.value)
-            $(buttons).removeAttr('disabled');
-        }
-        else{
-            el.value = null;
-            $(label).text('Invalid image type: ' + ext)
-            $(buttons).attr('disabled','disabled');
-        }
-      }
-    }
-}
-
-function upload_validate(el){
-    var ext = el.value.toLowerCase().split('.').pop();
-
-    if( ext in {'png':1,'jpeg':1,'jpg':1} ){
-        $(el.parentNode).find('label').text("Selected " + el.value)
-        $('.input-submit').removeAttr('disabled');
-    }
-    else{
-        el.value = null;
-        $(el.parentNode).find('label').text('Invalid image type: ' + ext)
-        $('.input-submit').attr('disabled','disabled');
-    }
-}
