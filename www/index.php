6b44f07b80fd92c684ce29e50f18b9298aff82cf
Matthijs van Henten <matthijs@ischen.nl>
2013-03-06 00:00:02 +0100

    add hack for magic_quotes

--- www/index.php
+++ www/index.php
@@ -4,5 +4,27 @@ ini_set('display_warnings', "true");
 ini_set('upload_max_filesize', '16M');
 ini_set('post_max_size', '16M');
 
+/**
+ * Stupid hosters leave this on, undo it before anything else for the sake
+ * of a coherent environment.
+ * 
+ * @TODO encapsulate this inside request obj.
+ */
+if (get_magic_quotes_gpc()) {
+    $process = array(&$_GET, &$_POST, &$_COOKIE, &$_REQUEST);
+    while (list($key, $val) = each($process)) {
+        foreach ($val as $k => $v) {
+            unset($process[$key][$k]);
+            if (is_array($v)) {
+                $process[$key][stripslashes($k)] = $v;
+                $process[] = &$process[$key][stripslashes($k)];
+            } else {
+                $process[$key][stripslashes($k)] = stripslashes($v);
+            }
+        }
+    }
+    unset($process);
+}
+
 require_once( '../Application/Bootstrap.php' );
 ?>
